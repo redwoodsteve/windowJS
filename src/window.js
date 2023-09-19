5b5ead8109b8be248fedf13817b832f8ce7f303c
redwoodsteve <80862755+redwoodsteve@users.noreply.github.com>
2023-09-18 19:33:45 -0700

    Create window.js

---
+++ src/window.js
@@ -0,0 +1,11 @@
+// window.js
+// created by redwoodsteve
+// https://github.com/redwoodsteve/windowJS
+function mask(URL, replace) {
+    var page = window.open(); //Open a new window
+    page.document.open(); //Open the document
+    page.document.write('<html><iframe src="' + URL + '" style="position: fixed;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 999999;height: 100%;"></iframe></html>'); //Write an IFrame into the new tab
+    window.location.replace(replace); //Replace the current tab
+    page.document.close();
+    document.close()
+}
